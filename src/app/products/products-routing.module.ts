e28e5acc850ca5398d0a5095a26979c1833052ae
Nailton Simões <nailtonsimoes@gmail.com>
2021-12-30 03:37:14 -0300

    commit com rotas e componentes criados

---
+++ src/app/products/products-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CreateProductComponent } from './create-product/create-product.component';
+import { DeleteProductComponent } from './delete-product/delete-product.component';
+import { ProductsComponent } from './products.component';
+import { UpdateProductComponent } from './update-product/update-product.component';
+import { ViewAllProductsByCategoryComponent } from './view-all-products-by-category/view-all-products-by-category.component';
+import { ViewAllProductsByDateComponent } from './view-all-products-by-date/view-all-products-by-date.component';
+import { ViewAllProductsComponent } from './view-all-products/view-all-products.component';
+import { ViewProductComponent } from './view-product/view-product.component';
+
+const routes: Routes = [
+  { path: '', component: ProductsComponent },
+  { path: 'create-product', component: CreateProductComponent },
+  { path: 'delete-product', component: DeleteProductComponent },
+  { path: 'update-product', component: UpdateProductComponent },
+  { path: 'view-product', component: ViewProductComponent },
+  { path: 'view-all-products', component: ViewAllProductsComponent },
+  { path: 'view-all-products-by-category', component: ViewAllProductsByCategoryComponent },
+  { path: 'view-all-products-by-date', component: ViewAllProductsByDateComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ProductsRoutingModule { }
