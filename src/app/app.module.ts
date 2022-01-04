ec0e37af587229e1bd08d83db074591add21e745
Nailton Simões <nailtonsimoes@gmail.com>
2022-01-04 18:11:12 -0300

    push com criação e configuração inicial do layout do e-comerce

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { OrdersModule } from './orders/orders.module'
+import { SiteFrameworkModule } from './site-framework/site-framework.module'
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { OrdersModule } from './orders/orders.module'
   imports: [
     BrowserModule,
     AppRoutingModule,
-    OrdersModule
+    OrdersModule,
+    SiteFrameworkModule
   ],
   providers: [],
   bootstrap: [AppComponent]
